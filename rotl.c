e4a6a792fe82fdfa31a7f97f4fa8616e4cdf87f5
Brainstorma <brainstormabaaj@gmail.com>
2023-01-19 16:51:36 -0800

    automated commit

--- rotl.c
+++ rotl.c
@@ -3,18 +3,18 @@
   *f_rotl- rotates the stack to the top
   *@head: stack head
   *@counter: line_number
-  *Return: no return
- */
+  *Return: void
+ **/
 void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
 {
-	stack_t *tmp = *head, *aux;
+	stack_t *tmp = *head, *sum;
 
 	if (*head == NULL || (*head)->next == NULL)
 	{
 		return;
 	}
-	aux = (*head)->next;
-	aux->prev = NULL;
+	sum = (*head)->next;
+	sum->prev = NULL;
 	while (tmp->next != NULL)
 	{
 		tmp = tmp->next;
@@ -22,5 +22,5 @@ void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
 	tmp->next = *head;
 	(*head)->next = NULL;
 	(*head)->prev = tmp;
-	(*head) = aux;
+	(*head) = sum;
 }
